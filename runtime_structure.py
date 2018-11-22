ba19bfc185c930c3c2e898ea23cc952b294e4e65
Wang Dong <wangdong17@foxmail.com>
2018-11-22 15:54:17 +0800

    Add construction code segment.

--- runtime_structure.py
+++ runtime_structure.py
@@ -1,11 +1,17 @@
 import struct
+from utils import *
+from instructions import *
 
 value_type = {0x7f: 'i32', 0x7e: 'i64', 0x7d: 'f32', 0x7c: 'f64'}
+import_kind = {0x00: 'func', 0x01: 'table', 0x02: 'mem', 0x03: 'global'}
+export_kind = {0x00: 'func', 0x01: 'table', 0x02: 'mem', 0x03: 'global'}
+table_type = {0x70: 'anyfunc'}
 
 class Module(object):
     def __init__(self, bytecode):
         self.types = []
         self.funcs = []
+        self.codes = []
         self.tables = []
         self.mems = []
         self.globals = []
@@ -20,8 +26,15 @@ class Module(object):
         pos += 8
 
         # Construct section 'Type'
-        section_code, section_size, num = struct.unpack('b', bytecode[pos:pos+3])
-        pos += 3
+        section_code = struct.unpack('b', bytecode[pos])[0][0]
+        pos += 1
+
+        section_size, byte_move = read_u32(bytecode, pos) # TODO: ERROR if return -1
+        pos += byte_move
+
+        num, byte_move = read_u32(bytecode[pos])
+        pos += byte_move
+    
         for i in range(num):
             # func_type = bytecode[pos]  # reserved for future
             params_num = struct.unpack('b', bytecode[pos+1])[0]
@@ -30,14 +43,156 @@ class Module(object):
             results_type = list(struct.unpack('b'*results_num, bytecode[pos+3+params_num:pos+3+params_num+results_num]))
             params_type = [value_type[params_type[j]] for j in range(len(params_type))]  # convert the format hex to str
             results_type = [value_type[results_type[j]] for j in range(len(results_type))]
-            self.types.append(Type(params_type, results_type))
             pos += 3 + params_num + results_num
+            self.types.append(Type(params_type, results_type))
 
         # Construct section 'Import'
-        section_code, section_size, num = struct.unpack('b', bytecode[pos:pos+3])
-        pos += 3
-        for i in range(num):
+        section_code = struct.unpack('b', bytecode[pos])[0]
+        pos += 1
+        
+        section_size, byte_move = read_u32(bytecode, pos)
+        pos += byte_move
 
+        num, byte_move = read_u32(bytecode, pos)
+        pos += byte_move
+
+        for i in range(num):
+            str_len, byte_move = read_u32(bytecode, pos)
+            pos += byte_move
+            module_name = bytecode[pos:pos+str_len].decode('utf8')
+            pos += str_len
+
+            str_len, byte_move = read_u32(bytecode, pos)
+            pos += byte_move
+            field_name = bytecode[pos:pos+str_len].decode('utf8')
+            pos += str_len
+
+            kind = import_kind[struct.unpack('b', bytecode[pos])[0]]
+            pos += 1
+            if kind == 'func':
+                funcidx, byte_move = read_u32(bytecode, pos)
+                pos += byte_move
+            else:
+                pass # TODO: table, mem, global
+
+            self.imports.append(Import(kind, funcidx, [module_name, field_name]))
+
+        # Construct section 'Function'
+        section_code = struct.unpack('b', bytecode[pos])[0]
+        pos += 1
+
+        section_size, byte_move = read_u32(bytecode, pos)
+        pos += byte_move
+
+        num, byte_move = read_u32(bytecode, pos)
+        pos += byte_move
+
+        for i in range(num):
+            funcidx, byte_move = read_u32(bytecode, pos)
+            pos += byte_move
+            self.funcs.append(funcidx)
+
+        # Construct section 'Table'
+        section_code = struct.unpack('b', bytecode[pos])[0]
+        pos += 1
+
+        section_size, byte_move = read_u32(bytecode, pos)
+        pos += byte_move
+
+        num, byte_move = read_u32(bytecode, pos)
+        pos += byte_move
+
+        for i in range(num):
+            elemtype, flag = struct.unpack('bb', bytecode[pos:pos+2]) # elemtype is 0x70 at present
+            pos += 2
+
+            if flag == 0x00:
+                min_limit, byte_move = read_u32(bytecode, pos)
+                max_limit = None
+                pos += byte_move
+            elif flag == 0x01:
+                min_limit, byte_move = read_u32(bytecode, pos)
+                pos += byte_move
+                max_limit, byte_move = read_u32(bytecode, pos)
+                pos += byte_move
+            else:
+                pass  # TODO: Error processing
+
+            self.tables.append(Table(elemtype, [min_limit, max_limit]))
+
+        # Construct section 'Memory'
+        section_code = struct.unpack('b', bytecode[pos])[0]
+        pos += 1
+
+        section_size, byte_move = read_u32(bytecode, pos)
+        pos += byte_move
+
+        num, byte_move = read_u32(bytecode, pos)
+        pos += byte_move
+
+        for i in range(num):
+            flag = struct.unpack('b', bytecode[pos])[0]
+            pos += 1
+
+            if flag == 0x00:
+                min_limit, byte_move = read_u32(bytecode, pos)
+                max_limit = None
+                pos += byte_move
+            elif flag == 0x01:
+                min_limit, byte_move = read_u32(bytecode, pos)
+                pos += byte_move
+                max_limit, byte_move = read_u32(bytecode, pos)
+                pos += byte_move
+            else:
+                pass
+
+        self.mems.append([min_limit, max_limit])
+
+        # Construct section 'Export'
+        section_code = struct.unpack('b', bytecode[pos])[0]
+        pos += 1
+
+        section_size, byte_move = read_u32(bytecode, pos)
+        pos += byte_move
+
+        num, byte_move = read_u32(bytecode, pos)
+        pos += byte_move
+
+        for i in range(num):
+            str_len, byte_move = read_u32(bytecode, pos)
+            pos += byte_move
+            name = bytecode[pos:pos+str_len].decode('utf8')
+            pos += str_len
+
+            kind = export_kind[struct.unpack('b', bytecode[pos])[0]]
+            pos += 1
+
+            sigidx, byte_move = read_u32(bytecode, pos)
+            pos += byte_move
+            
+            self.exports.append(Export(kind, sigidx, name))
+            
+        # Construct section 'Elem'
+        section_code = struct.unpack('b', bytecode[pos])[0]
+        pos += 1
+
+        section_size, byte_move = read_u32(bytecode, pos)
+        pos += byte_move
+
+        num, byte_code = read_u32(bytecode, pos)
+        pos += byte_move
+
+        for i in range(num):
+            tableidx, byte_move = read_u32(bytecode, pos)
+            pos += byte_move
+
+            op = opcode[struct.unpack('B', bytecode[pos])]
+            pos += 1
+            if op in operand_u32:
+                offset, byte_move = read_u32(bytecode, pos)
+            else:
+                offset, byte_move = read_u64(bytecode, pos)
+            pos += byte_move
 
 
 class Type(object):
@@ -46,5 +201,35 @@ class Type(object):
         self.results_type = results_type
         self.func_type = func_type
 
+
+
 class Import(object):
-    def __init__(self, )
+    def __init__(self, kind, sigidx=None, name=None):
+        self.kind = kind
+        self.sigidx = sigidx
+        self.name = name
+
+class Table(object):
+    def __init__(self, elemtype, limit):
+        self.elemtype = elemtype
+        self.limit = limit
+
+class Memory(object):
+    def __init__(self, limit):
+        self.limit = limit
+        self.space = [] # Initial empty space
+
+class Export(object):
+    def __init__(self， kind, sigidx=None, name=None):
+        self.kind = kind
+        self.sigidx = sigidx
+        self.name = name
+
+class Elem(object):
+    def __init__(self, tableidx, expr=None, funcidx=None):
+        self.tableidx = tableidx
+        self.expr = expr
+        self.funcidx = funcidx
+
+class Code(object):
+    def __init__(self, ):
--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import six
-
+import struct
 
 # memory arguments
 class memarg(object):
@@ -13,4 +13,27 @@ def is_real(value):
 def is_symbolic(value):
     return not isinstance(value, six.integer_types)
 
-def 
+def read_u32(bytecode, pos):
+    first, second, third, fourth = struct.unpack('BBBB', bytecode[pos:pos+5])
+    if (first & 0x80) == 0:
+        size = first & 0x7f
+        byte_move = 1
+    elif (second & 0x80) == 0:
+        size = ((first & 0x7f) << 7) | (first & 0x7f)
+        byte_move = 2
+    elif (third & 0x80) == 0:
+        size = ((third & 0x7f) << 14) | ((second & 0x7f) << 7) | (first & 0x7f)
+        byte_move = 3
+    elif (fourth & 0x80) == 0:
+        size = ((fourth & 0x7f) << 21) | ((third & 0x7f) << 14) | ((second & 0x7f) << 7) | (first & 0x7f)
+        byte_move = 4
+    elif (bytecode[pos+4] & 0x80) == 0:
+        if bytecode[pos+4] & 0xf0:
+            return (-1, -1)  # The size of bits > 32bits
+        size = ((bytecode[pos+4] & 0x7f) << 28) | ((fourth & 0x7f) << 21) | ((third & 0x7f) << 14) | ((second & 0x7f) << 7) | (first & 0x7f)
+        byte_move = 5
+    else:
+        size = -1
+        byte_move = -1
+    
+    return (size, byte_move)
